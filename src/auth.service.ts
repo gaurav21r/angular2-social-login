381b9344fed97beb3af3fe99df984ca26725f602
Gaurav Ramanan <gaurav@rcorp.co.in>
2019-05-11 17:52:53 +0530

    Upgraded for new RxJs

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Observable";
-import { Observer } from "rxjs/Observer";
+import { Observable } from "rxjs";
+import { Observer } from "rxjs";
 
 declare let gapi: any;
 declare let IN: any;
