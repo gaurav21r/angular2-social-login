a671baf21336c58376235694b4650ba5ecc958fc
Gaurav Ramanan <gaurav@rcorp.co.in>
2019-05-11 18:25:23 +0530

    Updated for latest version of RxJs

--- dist/auth.service.js
+++ dist/auth.service.js
@@ -5,7 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs";
 var AuthService = (function () {
     function AuthService() {
     }
